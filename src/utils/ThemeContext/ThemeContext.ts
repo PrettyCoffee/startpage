f0290c34973945cadffdabb131ac0d5a4177fe2c
PrettyCoffee <jonathan-holz@outlook.com>
2021-08-24 01:23:05 +0200

    feat(ThemeContext): Implement ThemeContext

---
+++ src/utils/ThemeContext/ThemeContext.ts
@@ -0,0 +1,13 @@
+import { createContext } from "react"
+
+import { getTheme, Theme } from "../../base/theme"
+
+export type ThemeState = [Theme, (theme: Theme) => void]
+
+const defaultTheme: ThemeState = [getTheme(), () => null]
+
+/** Context for global theme accessing.
+ * Use the `ThemeProvider` to use the context.
+ * The theme can be accessed by using the `useTheme` hook while inside the ThemeProvider.
+ */
+export const ThemeContext = createContext(defaultTheme)
