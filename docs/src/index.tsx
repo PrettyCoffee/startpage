d811a821ccd8bbe28e37612b2d916bb0a38fc233
PrettyCoffee <jonathan-holz@outlook.com>
2021-09-18 21:12:36 +0200

    docs: Add Headline

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { css } from "@emotion/react"
+import styled from "@emotion/styled"
 import { render } from "react-dom"
 
 import { ColorPreview } from "./ColorPreview/ColorPreview"
@@ -7,9 +9,17 @@ import { PageLayout } from "./layout/PageLayout"
 import * as Examples from "./pages"
 import { Providers } from "./Providers"
 
+const Headline = styled.h1`
+  ${({ theme: { color, space } }) => css`
+    font-size: ${space.largest};
+    color: ${color.fg.base};
+  `}
+`
+
 const App = () => (
   <Providers>
     <PageLayout>
+      <Headline>react-startpage</Headline>
       <ColorPreview />
       {Object.values(Examples).map(Example => (
         <Example />
