2c9e5c067bd2c6c96a26360b309449e24746e038
PrettyCoffee <jonathan-holz@outlook.com>
2021-09-18 19:37:16 +0200

    docs(PageLayout): Introduce Layout

---
+++ docs/src/layout/PageLayout.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+import { css } from "@emotion/react"
+import styled from "@emotion/styled"
+
+import { Header } from "./Header"
+
+const Layout = styled.main`
+  display: grid;
+  grid-template-areas: ". main .";
+  grid-template-columns: auto auto auto;
+`
+
+const Content = styled.div`
+  ${({ theme: { space } }) => css`
+    grid-area: main;
+    max-width: 1600px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: ${space.largest};
+  `}
+`
+
+export const PageLayout = ({ children }: React.PropsWithChildren<unknown>) => (
+  <>
+    <Header />
+    <Layout>
+      <Content>{children}</Content>
+    </Layout>
+  </>
+)
