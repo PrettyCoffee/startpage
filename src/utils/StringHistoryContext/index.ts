6723a0fa560e37a5569d4290f6514068c161b7ac
PrettyCoffee <jonathan-holz@outlook.com>
2021-09-21 01:48:48 +0200

    refactor: Remove old files

--- src/utils/StringHistoryContext/index.ts
+++
@@ -1,9 +0,0 @@
-export { StringHistoryContext } from "./StringHistoryContext"
-// eslint-disable-next-line prettier/prettier
-export type { StringHistoryState } from "./StringHistoryContext"
-
-export { StringHistoryProvider } from "./StringHistoryProvider"
-// eslint-disable-next-line prettier/prettier
-export type { StringHistoryProviderProps } from "./StringHistoryProvider"
-
-export { useStringHistory } from "./useStringHistory"
--- src/utils/StringHistoryContext/useStringHistory.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-
-import { StringHistoryContext } from "./StringHistoryContext"
-
-/** Hook for accessing the `StringHistoryContext`.
- * Can only be used if an `StringHistoryProvider` is a Parent component.
- * @returns the history object which is provided by the nearest `StringHistoryProvider`
- */
-export const useStringHistory = () => useContext(StringHistoryContext)
