94fe45befd927730c63c3538eff196ad1b89ddaf
PrettyCoffee <jonathan-holz@outlook.com>
2021-10-01 00:05:12 +0200

    feat(search): Add performSearch function

--- packages/search/src/index.ts
+++ packages/search/src/index.ts
@@ -1,1 +1,3 @@
-export {} from ""
+export { performSearch } from "./performSearch"
+export type { SearchOptions } from "./performSearch"
+export type { SearchEngineName } from "./utils/getEngineUrl"
