39229fe179b27c921a7374e8a5b540bba30e6425
PrettyCoffee <jonathan-holz@outlook.com>
2021-10-01 00:49:21 +0200

    fix: Build problems

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -2,8 +2,6 @@ import React from "react"
 
 import { render } from "react-dom"
 
-const App = () => (
-  <h1>Documentation</h1>
-)
+const App = () => <h1>Documentation</h1>
 
 render(<App />, document.getElementById("root"))
