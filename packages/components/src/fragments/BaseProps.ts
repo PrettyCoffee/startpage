8a3430530da01ddb7671cb2953179ceb92ef4c58
PrettyCoffee <jonathan-holz@outlook.com>
2021-10-05 00:30:37 +0200

    refactor(components): Extract FocusProps

--- packages/components/src/fragments/BaseProps.ts
+++ packages/components/src/fragments/BaseProps.ts
@@ -1,12 +1,5 @@
 import React from "react"
 
-export type ExtraProps = {
-  /** Component which is displayed on the left side */
-  leftExtra?: React.ReactFragment
-  /** Component which is displayed on the right side */
-  rightExtra?: React.ReactFragment
-}
-
 export type ClassNameProp = {
   /** Pass your own css classes for unique styling. */
   className?: string
@@ -17,14 +10,23 @@ export type DisableProp = {
   disabled?: boolean
 }
 
+export type ExtraProps = {
+  /** Component which is displayed on the left side */
+  leftExtra?: React.ReactFragment
+  /** Component which is displayed on the right side */
+  rightExtra?: React.ReactFragment
+}
+
+export type FocusProps<ElementType> = {
+  /** Callback fired when the element gains focus */
+  onFocus?: React.FocusEventHandler<ElementType>
+  /** Callback fired when the element looses focus */
+  onBlur?: React.FocusEventHandler<ElementType>
+}
+
 export type BaseInputProps<ElementType, ValueType> = ClassNameProp &
-  DisableProp & {
+  DisableProp &
+  FocusProps<ElementType> & {
     /** Callback fired when the elements value changes */
     onChange?: (value: ValueType, event: React.ChangeEvent<ElementType>) => void
-    /** Indicates focusability and position in keyboard navigation */
-    tabIndex?: number
-    /** Callback fired when the element gains focus */
-    onFocus?: React.FocusEventHandler<ElementType>
-    /** Callback fired when the element looses focus */
-    onBlur?: React.FocusEventHandler<ElementType>
   }
