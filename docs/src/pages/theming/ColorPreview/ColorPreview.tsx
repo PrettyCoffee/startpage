ed7d9ebfaf30d1398145caa80f0efb8a0540dd16
PrettyCoffee <jonathan-holz@outlook.com>
2021-10-23 03:29:19 +0200

    docs(ColorPreview): Use TabGroup component

--- docs/src/pages/theming/ColorPreview/ColorPreview.tsx
+++ docs/src/pages/theming/ColorPreview/ColorPreview.tsx
@@ -2,33 +2,27 @@ import React from "react"
 
 import { css, ThemeProvider as EmotionThemeProvider } from "@emotion/react"
 import styled from "@emotion/styled"
+import { Tab } from "@startpage/components"
 import { getTheme, ColorSchemeName } from "@startpage/theming"
 
-import { Button } from "../../../components"
+import { TabGroup } from "../../../components"
 import { ThemeToggle } from "../../../components/ThemeToggle"
 import { ColorPanel } from "./ColorPanel"
 
-const Container = styled.div`
-  width: fit-content;
-  margin: 0 auto;
+const ThemeToggleLayout = styled.div`
+  ${({ theme: { space } }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: ${space.largest};
+    display: flex;
+    align-items: center;
+  `}
 `
 
-const ButtonPanel = styled.div`
-  ${({ theme: { space } }) => css`
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    margin: ${space.medium} 0;
-  `}
-`
-const ThemeButtons = styled.div`
-  ${({ theme: { space } }) => css`
-    display: flex;
-    > button {
-      margin-right: ${space.small};
-    }
-  `}
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
 `
 
 const availableThemes: ColorSchemeName[] = [
@@ -38,28 +32,25 @@ const availableThemes: ColorSchemeName[] = [
   "material",
 ]
 
+const themes = ["atom", "dracula", "nord", "material"]
+
 export const ColorPreview = () => {
   const [currentTheme, setCurrentTheme] = React.useState(availableThemes[0])
   const [inverted, setInverted] = React.useState(false)
   return (
     <EmotionThemeProvider theme={getTheme(currentTheme, inverted)}>
-      <Container>
-        <ButtonPanel>
-          <ThemeButtons>
-            {availableThemes.map(theme => (
-              <Button
-                key={theme}
-                active={currentTheme === theme}
-                onClick={() => setCurrentTheme(theme)}
-              >
-                {theme}
-              </Button>
-            ))}
-          </ThemeButtons>
+      <Wrapper>
+        <ThemeToggleLayout>
           <ThemeToggle inverted={inverted} onChange={setInverted} />
-        </ButtonPanel>
-        <ColorPanel theme={getTheme(currentTheme, inverted)} />
-      </Container>
+        </ThemeToggleLayout>
+        <TabGroup onChange={index => setCurrentTheme(themes[index])}>
+          {themes.map(theme => (
+            <Tab key={theme} title={theme}>
+              <ColorPanel theme={getTheme(theme, inverted)} />
+            </Tab>
+          ))}
+        </TabGroup>
+      </Wrapper>
     </EmotionThemeProvider>
   )
 }
